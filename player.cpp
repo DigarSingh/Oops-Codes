24d4d313adc998a908099ce2ea9ce08838a654a5
BYTE90 <digarsingh2004@gmail.com>
2025-03-12 00:03:47 +0530

    player

--- player.cpp
+++ player.cpp
@@ -2,29 +2,53 @@
 using namespace std;
 
 class player{  // class definition
+
      private:  // private access specifier
+     
      int score; // private data member
      int health;
+     int age;
+     bool alive;
 
      public: // public access specifier
-
-     void setScore(int s){    // public member function
-          score = s;     // accessing private data member
+     
+    
+     int getScore(){     // getter for returning values
+          return score;
      }
-     void setHealth(int h){
-          health = h;   
+     int getHealth(){
+          return health;
      }
-     void showHealth(){  
-          cout<<"Health is: "<<health<<endl; 
+     int getAge(){
+          return age;
      }
-     void showScore(){   
-          cout<<"Score is: "<<score<<endl; 
+     int IsAlive(){
+          return alive;
      }
+     // public members function
+     void setHealth(int health){    // setter for inserting values 
+         this->health = health;     // this matlab iss wali class ka health
+     }
+     void setScore(int score){
+         this->score = score;       // accessing private data members
+     }
+     void setAge(int age){
+          this->age = age;
+     }
+     void setIsAlive(bool alive){
+          this->alive = alive;
+     } 
 };
 int main(){
      player p; // creating object of class player
+
      p.setHealth(100);   // accessing public member function
-     p.showHealth();
-     p.setScore(10);    
-     p.showScore(); 
+     p.setScore(10);
+     p.setAge(18);
+     p.setIsAlive(1);   
+     cout<<p.getScore()<<endl;
+     cout<<p.getHealth()<<endl;
+     cout<<p.getAge()<<endl;
+     cout<<p.IsAlive()<<endl;
+
 }
